88723e32ef5341fc86bd7a6f80263e0d114f1c0d
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2022-11-02 15:43:53 +0800

    added lists in index

--- app.js
+++ app.js
@@ -10,8 +10,36 @@ app.set('view engine', 'pug');
 
 // home route
 app.get('/', (req, res) => {
+    let articles = [
+        {
+            id: 1,
+            title: 'Article 1',
+            author: 'Ethan Chen',
+            body: 'This is article 1'
+        },
+        {
+            id: 2,
+            title: 'Article 2',
+            author: 'Ethan Chen',
+            body: 'This is article 2'
+        },
+        {
+            id: 3,
+            title: 'Article 3',
+            author: 'Ethan Chen',
+            body: 'This is article 3'
+        },
+        {
+            id: 4,
+            title: 'Article 4',
+            author: 'Ethan Chen',
+            body: 'This is article 4'
+        },
+    ]
+
     res.render('index', {
-        title: 'Index'
+        title: 'Index',
+        articles: articles
     });
     console.log('Someone logged on');
 })
