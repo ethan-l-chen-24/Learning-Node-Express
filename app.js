39eebfe49246c0f8f5397d6c7c87fddfe27e90ca
ethanlchen <ethan.l.chen.24@dartmouth.edu>
2022-11-03 17:56:55 +0800

    adding bootstrap

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ let Article = require('./models/article');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+ 
 // load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
